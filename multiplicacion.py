05dc6887fccc1d89c27eeba5eae07ddc405a925d
libra123451 <aderlyaldair@gmail.com>
2025-02-26 12:57:30 -0500

    listo

---
+++ multiplicacion.py
@@ -0,0 +1,6 @@
+primerNumero = int(input("Ingrese un 1 numero: "))
+segundoNumero = int(input("Ingrese un 2 numero: "))
+
+muntiplicacion=(primerNumero * segundoNumero)
+print(muntiplicacion)
+# yei
