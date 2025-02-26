27afeeb63f75b33881272dc6d827958a0c4a6b6f
Jesu513 <jesualbert2004@gmail.com>
2025-02-26 12:44:00 -0500

    chore: agregue la operacion resta

--- suma.py
+++ suma.py
@@ -2,6 +2,7 @@ primerNumero = int(input("Ingrese un 1 numero: "))
 segundoNumero = int(input("Ingrese un 2 numero: "))
 
 suma = (primerNumero + segundoNumero)
-
+resta = (primerNumero - segundoNumero)
 print(suma)
+print(resta)
 # yei
